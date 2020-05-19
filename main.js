29e7bf006379185e2b228bfac6350281cc9b119d
Jarek Lewandowski <jarek.lewandowski@gmail.com>
2020-05-19 11:25:36 +0200

    Duża aktualizacja

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const firstName = 'Jarek';
+const firstName = "Jarek";
 const age = 38;
-
 
 console.log(firstName);
 console.log(age);
 
-console.log(`Nazywam się ${firstName} i mam ${age} lat.`)
+console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
+
+const heading = document.querySelector('.footer--js');
+heading.innerHTML = 'Stopka dokumentu. &copy; 2020';
+//console.log(heading.innerHTML);
